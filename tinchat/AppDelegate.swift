5c9252651101800fce1e39851cc1f2ec62dfecad
Nate-Taylor <natetaylor.ggl@gmail.com>
2017-09-24 09:20:34 +0300

    1st homework

---
+++ tinchat/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  tinchat
+//
+//  Created by nate_taylor on 24.09.17.
+//  Copyright © 2017 nate_taylor. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print("Application moved from [Not running] to [Foreground-inactive] state: \(#function)\n")
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("Application moved from [Foreground-active] to [Foreground-inactive] state: \(#function)\n")
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("Application moved from [Foreground-inactive] to [Background] state: \(#function)\n")
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("Application moved from [Background] to [Foreground-inactive] state: \(#function)\n")
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("Application moved from [Foreground-inactive] to [Foreground-active] state: \(#function)\n")
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("Application moved from [Background] to [Not running] state: \(#function)\n")
+    }
+
+
+}
+
